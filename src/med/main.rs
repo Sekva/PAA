9919c6df1c477fc599467d6a0037444a1da126de
Sekva <matheusmachado708@gmail.com>
2019-01-18 23:53:20 -0300

    Saida iterativa ok, porem a recursiva com defeito

--- src/med/main.rs
+++ src/med/main.rs
@@ -1,19 +1,22 @@
 // Melhor exemplo:
 // let str1 = String::from("atraocaasdqw");
-// let str2 = String::from("troacabdwqoi");
+// let str2 = String::from("rtoacabdwqoi");
+// http://odur.let.rug.nl/kleiweg/lev/
 
 // Não tem a função de eliminar, já que a inserção/deleção
 // do restante dos caracteres faz mais sentido
 
 fn main() {
     let str1 = String::from("atraocaasdqw");
-    let str2 = String::from("troacabdwqoi");
+    let str2 = String::from("rtoacabdwqoi");
     let n = str1.len() as i32 - 1;
     let m = str2.len() as i32 - 1;
     let saida_iterativa = med_iterativo(&str1, &str2);
     println!("saida itetativa : {}", saida_iterativa);
     let saida_recursiva = init_med_recursivo(&str1, &str2, n, m);
     println!("saida recursiva : {}", saida_recursiva);
+    let saida_recursiva_sem_memoria = med_recursivo_desmemoriado_e_lerdo(&str1, &str2, n, m);
+    println!("saida saida_recursiva_sem_memoria : {}", saida_recursiva_sem_memoria);
 }
 
 const MODIFICADOR_LEVENSHTEIN: i32 = 2; // CUSTO DE SUBSTITUIÇÃO
@@ -38,11 +41,24 @@ fn med_recursivo_desmemoriado_e_lerdo(str1: &String, str2: &String, i: i32, j: i
         return med_recursivo_desmemoriado_e_lerdo(str1, str2, i - 1, j - 1) + CUSTO_COPIAR;
     }
 
-    return min(
+    let mut custo = min(
         med_recursivo_desmemoriado_e_lerdo(str1, str2, i - 1, j) + CUSTO_DELETAR,
         med_recursivo_desmemoriado_e_lerdo(str1, str2, i, j - 1) + CUSTO_INSERIR,
         MODIFICADOR_LEVENSHTEIN - 1 + med_recursivo_desmemoriado_e_lerdo(str1, str2, i - 1, j - 1),
     );
+
+    if i > 1
+        && j > 1
+        && str1.chars().nth(i as usize) == str2.chars().nth(j as usize - 1)
+        && str1.chars().nth(i as usize - 1) == str2.chars().nth(j as usize)
+    {
+        custo =  std::cmp::min(
+            custo,
+            med_recursivo_desmemoriado_e_lerdo(str1, str2, i-2, j-2) - 1 + CUSTO_SWAP
+        );
+    }
+
+    return custo;
 
     // Sem codigo de swap, é o mesmo do com memoria
 }
@@ -64,46 +80,30 @@ fn med_recursivo(str1: &String, str2: &String, i: i32, j: i32, memoria: &mut Vec
     }
 
     if i == 0 {
-        *memoria
-            .get_mut(i as usize)
-            .unwrap()
-            .get_mut(j as usize)
-            .unwrap() = j;
+        *memoria.get_mut(i as usize).unwrap().get_mut(j as usize).unwrap() = j;
         return j;
     }
 
     if j == 0 {
-        *memoria
-            .get_mut(i as usize)
-            .unwrap()
-            .get_mut(j as usize)
-            .unwrap() = i;
+        *memoria.get_mut(i as usize).unwrap().get_mut(j as usize).unwrap() = i;
         return i;
     }
 
     if str1.chars().nth(i as usize - 1) == str2.chars().nth(j as usize - 1) {
-        *memoria
-            .get_mut(i as usize)
-            .unwrap()
-            .get_mut(j as usize)
-            .unwrap() = med_recursivo(str1, str2, i - 1, j - 1, memoria) + CUSTO_COPIAR;
+        *memoria.get_mut(i as usize).unwrap().get_mut(j as usize).unwrap() = med_recursivo(str1, str2, i - 1, j - 1, memoria) + CUSTO_COPIAR;
         return med_recursivo(str1, str2, i - 1, j - 1, memoria);
     }
 
-    *memoria
-        .get_mut(i as usize)
-        .unwrap()
-        .get_mut(j as usize)
-        .unwrap() = min(
-        med_recursivo(str1, str2, i - 1, j, memoria) + CUSTO_DELETAR,
-        med_recursivo(str1, str2, i, j - 1, memoria) + CUSTO_INSERIR,
-        MODIFICADOR_LEVENSHTEIN + med_recursivo(str1, str2, i - 1, j - 1, memoria),
-    );
+    *memoria.get_mut(i as usize).unwrap().get_mut(j as usize).unwrap() = min(
+            med_recursivo(str1, str2, i - 1, j, memoria) + CUSTO_DELETAR,
+            med_recursivo(str1, str2, i, j - 1, memoria) + CUSTO_INSERIR,
+            MODIFICADOR_LEVENSHTEIN + med_recursivo(str1, str2, i - 1, j - 1, memoria) - 1,
+        );
 
-    if (i > 1
+    if i > 1
         && j > 1
         && str1.chars().nth(i as usize) == str2.chars().nth(j as usize - 1)
-        && str1.chars().nth(i as usize - 1) == str2.chars().nth(j as usize))
+        && str1.chars().nth(i as usize - 1) == str2.chars().nth(j as usize)
     {
         *memoria
             .get_mut(i as usize)
@@ -154,10 +154,10 @@ fn med_iterativo(str1: &String, str2: &String) -> i32 {
                 MODIFICADOR_LEVENSHTEIN + distancias[i - 1][j - 1], // Substitui
             );
 
-            if (i > 1
+            if i > 1
                 && j > 1
                 && str1.chars().nth(i - 1) == str2.chars().nth(j - 2)
-                && str1.chars().nth(i - 2) == str2.chars().nth(j - 1))
+                && str1.chars().nth(i - 2) == str2.chars().nth(j - 1)
             {
                 distancias[i][j] = std::cmp::min(
                     distancias[i][j],                      //Mantem o mesmo valor
